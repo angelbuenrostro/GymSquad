087be02017f15644d7bcda5f536a8db268db0e59
Angel <angelbuenrostro@outlook.com>
2019-03-22 05:13:17 -0400

    Edit file structure

--- GymSquad/GymSquad/Models/Workout.swift
+++ GymSquad/GymSquad/Models/Workout.swift
@@ -6,4 +6,31 @@
 //  Copyright © 2019 Angel Buenrostro. All rights reserved.
 //
 
+
 import Foundation
+import UIKit
+import FirebaseFirestore
+
+
+struct Workout {
+    var name: String
+    var exercises: [Exercise]
+    
+    var dictionary: [String:Any] {
+        return [
+            "name": name,
+            "exercises": exercises
+        ]
+    }
+}
+
+extension Workout: DocumentSerializable {
+    init?(dictionary: [String:Any]){
+        guard let name = dictionary["name"] as? String,
+            let exercises = dictionary["exercises"] as? [Exercise] else { return nil }
+        
+        self.init(name: name, exercises: exercises)
+        
+    }
+}
+
--- GymSquad/GymSquad/Views/AddWorkoutViewController.swift
+++ GymSquad/GymSquad/Views/AddWorkoutViewController.swift
@@ -13,7 +13,7 @@ import FirebaseAuth
 class AddWorkoutViewController: UIViewController {
     
     var db: Firestore!
-    var userArray: [User] = []
+    var exercises: [Exercise] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -33,44 +33,4 @@ class AddWorkoutViewController: UIViewController {
     }
     */
 
-    @IBOutlet weak var userNameTextField: UITextField!
-    @IBAction func signInTapped(_ sender: Any) {
-        if let phoneNumber = userNameTextField.text  {
-            
-            PhoneAuthProvider.provider().verifyPhoneNumber(phoneNumber, uiDelegate: nil) { (verificationID, error) in
-                if let error = error {
-                    print(error.localizedDescription)
-                    return
-                }
-                // Sign in using the verificationID and the code sent to the user
-                // ...
-                UserDefaults.standard.set(verificationID, forKey: "authVerificationID")
-                
-                // Create alert to have user enter in verification code
-                let verificationAlert = UIAlertController(title: "Verify Phone Number", message: "Enter Verification Code", preferredStyle: .alert)
-                
-                verificationAlert.addTextField{ (textField) in
-                    textField.placeholder = "Enter SMS code here"
-                }
-                
-                verificationAlert.addAction(UIAlertAction(title: "Submit", style: .default, handler: { (_) in
-                    // Receive verification code + ID
-                    guard let verificationCode = verificationAlert.textFields![0].text else { return }
-                    let credential = PhoneAuthProvider.provider().credential(
-                        withVerificationID: verificationID!,
-                        verificationCode: verificationCode)
-                    
-                    Auth.auth().signInAndRetrieveData(with: credential, completion: { (authResult, error) in
-                        if let error = error {
-                            print("error signing in:\(error.localizedDescription)")
-                            return
-                        }
-                        
-                        print("You have successfully signed in!")
-                    })
-                }))
-                self.present(verificationAlert, animated: true)
-            }
-        }
-    }
 }
